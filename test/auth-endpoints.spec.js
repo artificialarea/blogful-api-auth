3f29f13148ad30680dd9de1032c49dd50c33f301
Sacha Sedriks <sachased@gmail.com>
2020-08-19 22:57:52 -0700

    update everything related to protected endpoints with JWT. phew.

--- test/auth-endpoints.spec.js
+++ test/auth-endpoints.spec.js
@@ -3,7 +3,6 @@ const knex = require('knex')
 const jwt = require('jsonwebtoken')
 const app = require('../src/app')
 const helpers = require('./test-helpers')
-const supertest = require('supertest')
 
 
 describe('Auth Endpoints', function () {
